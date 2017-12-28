26951ca5ed2c0a2e8bdf55c63e379168ed7cd095
任庆伟 <renqw@asiainfo.com>
2017-12-28 15:35:02 +0800

    make  to track all changes in scope

--- src/scope.js
+++ src/scope.js
@@ -1,7 +1,8 @@
 /* jshint globalstrict: true */
 'use strict';
 
-function initWatchVal(){}
+function initWatchVal() {
+}
 
 function Scope() {
 
@@ -13,27 +14,35 @@ function Scope() {
 Scope.prototype.$watch = function (watchFn, listenerFn) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn || function(){ },
+        listenerFn: listenerFn || function () {
+        },
         last: initWatchVal
     };
 
     this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$$digestOnce = function () {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if(newValue !== oldValue){
+        if (newValue !== oldValue) {
             watcher.last = newValue;
             watcher.listenerFn(newValue,
                 (oldValue === initWatchVal ? newValue : oldValue),
                 self);
+            dirty = true;
         }
-
     });
+    return dirty;
 };
 
+Scope.prototype.$digest = function () {
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+    } while (dirty);
+};
 
