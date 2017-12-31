76bd3d265fc61ea714a3561589aad52ecfc8a6f2
Chinwe <renqingwei123@163.com>
2017-12-31 21:24:26 +0800

    $evalAsync - Deferred Execution

--- src/scope.js
+++ src/scope.js
@@ -8,6 +8,7 @@ function Scope() {
 
     this.$$watchers = [];
     this.$$lastDirtyWatch = null;
+    this.$$asyncQueue = [];
 
 
 }
@@ -25,9 +26,9 @@ Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     this.$$lastDirtyWatch = null;
 };
 
-Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq){
-    if(valueEq){
-        return _.isEqual(newValue,oldValue);
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
     } else {
         return newValue === oldValue ||
             (typeof newValue === 'number' && typeof oldValue === 'number' &&
@@ -41,14 +42,14 @@ Scope.prototype.$$digestOnce = function () {
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)){
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
             self.$$lastDirtyWatch = watcher;
             watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
             watcher.listenerFn(newValue,
                 (oldValue === initWatchVal ? newValue : oldValue),
                 self);
             dirty = true;
-        } else if(self.$$lastDirtyWatch === watcher) {
+        } else if (self.$$lastDirtyWatch === watcher) {
             return false;
         }
     });
@@ -60,23 +61,32 @@ Scope.prototype.$digest = function () {
     var dirty;
     this.$$lastDirtyWatch = null;
     do {
+        while (this.$$asyncQueue.length) {
+            var asyncTask = this.$$asyncQueue.shift();
+            asyncTask.scope.$eval(asyncTask.expression);
+        }
         dirty = this.$$digestOnce();
-        if(dirty && !(ttl--)){
+        if (dirty && !(ttl--)) {
             throw "10 digest iterations reached";
         }
     } while (dirty);
 };
 
-Scope.prototype.$eval = function(expr, locals){
+Scope.prototype.$eval = function (expr, locals) {
     return expr(this, locals);
 };
 
-Scope.prototype.$apply = function(expr){
+Scope.prototype.$apply = function (expr) {
     try {
         return this.$eval(expr);
     } finally {
         this.$digest();
     }
 };
+
+Scope.prototype.$evalAsync = function (expr) {
+    this.$$asyncQueue.push({scope: this, expression: expr});
+};
+
 
 
